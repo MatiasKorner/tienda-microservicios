bfb823dfd99f98753b78bfa88f74be54345808cd
MatiasKorner <mat.korner@duocuc.cl>
2026-05-17 20:03:33 -0400

    corrige configuracion mapstruct y feign

--- ms-catalogo/src/main/java/cl/factogames/catalogo/TiendaCatalogoApplication.java
+++ ms-catalogo/src/main/java/cl/factogames/catalogo/TiendaCatalogoApplication.java
@@ -3,14 +3,12 @@ package cl.factogames.catalogo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = "cl.factogames.catalogo.client")
 @SpringBootApplication
 public class TiendaCatalogoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TiendaCatalogoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaCatalogoApplication.class, args);
+    }
 }
