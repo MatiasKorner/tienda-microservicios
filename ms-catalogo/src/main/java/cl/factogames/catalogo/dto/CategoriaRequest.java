15ea4b2a99a65b1ed13dca484fe954676d58368a
MartinCastaneda-design <ma.castanedau@duocuc.cl>
2026-05-24 21:25:11 -0400

    Delete ms-catalogo directory

--- ms-catalogo/src/main/java/cl/factogames/catalogo/dto/CategoriaRequest.java
+++
@@ -1,9 +0,0 @@
-package cl.factogames.catalogo.dto;
-
-import lombok.Data;
-
-@Data
-public class CategoriaRequest {
-
-    private String nombre;
-}
--- ms-catalogo/src/main/java/cl/factogames/catalogo/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.factogames.catalogo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cl.factogames.catalogo.model.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
-
-}
